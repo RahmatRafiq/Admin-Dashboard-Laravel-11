2dbcf616a463d2e22bcb0daf61d142753571ea51
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-07-14 18:27:06 +0800

    chore: Remove unused models and migrations

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,27 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
-    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
 
-    Route::get('/admin', [DashboardController::class, 'dashboardAdmin'])->name('dashboard.admin');
+    Route::get('/admin', [\App\Http\Controllers\DashboardController::class, 'dashboardAdmin'])->name('dashboard.admin');
 
-    Route::resource('mbkm/about-mbkms', \App\Http\Controllers\AboutMbkmController::class);
+    Route::resource('mbkm/about-app', \App\Http\Controllers\AboutMbkmController::class);
 
-    Route::get('mbkm/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('mbkm/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('mbkm/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('admin/profile', [\App\Http\Controllers\ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('admin/profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('admin/profile', [\App\Http\Controllers\ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::resource('mbkm/admin/role-permissions/permission', \App\Http\Controllers\RolePermission\PermissionController::class);
-    Route::post('mbkm/admin/role-permissions/permission/json', [\App\Http\Controllers\RolePermission\PermissionController::class, 'json'])->name('permission.json');
+    Route::resource('admin/role-permissions/permission', \App\Http\Controllers\RolePermission\PermissionController::class);
+    Route::post('admin/role-permissions/permission/json', [\App\Http\Controllers\RolePermission\PermissionController::class, 'json'])->name('permission.json');
 
-    Route::resource('mbkm/admin/role-permissions/role', \App\Http\Controllers\RolePermission\RoleController::class);
-    Route::post('mbkm/admin/role-permissions/role/json', [\App\Http\Controllers\RolePermission\RoleController::class, 'json'])->name('role.json');
+    Route::resource('admin/role-permissions/role', \App\Http\Controllers\RolePermission\RoleController::class);
+    Route::post('admin/role-permissions/role/json', [\App\Http\Controllers\RolePermission\RoleController::class, 'json'])->name('role.json');
 
-    Route::resource('mbkm/admin/role-permissions/user', UserController::class);
-    Route::post('mbkm/admin/role-permissions/user/json', [UserController::class, 'json'])->name('user.json');
+    Route::resource('admin/role-permissions/user', UserController::class);
+    Route::post('admin/role-permissions/user/json', [UserController::class, 'json'])->name('user.json');
 
 });
 
